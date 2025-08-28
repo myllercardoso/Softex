4f046984fa8cde97b7a5e2e7379f994e86c45d29
Myller Cardoso <myller.mil@gmail.com>
2025-08-28 16:27:55 -0300

    Delete Softex/Softex directory

--- Softex/Softex/AULA2/ATV1-3/script.js
+++
@@ -1,6 +0,0 @@
-function mostrarMensagem() {
-    //Pega o parágrafo com id="mensagem"
-    const mostrarMensagem = document.getElementById("mensagem");
-    //Altera o conteúdo do parágrafo
-    mostrarMensagem.textContent = "Parabéns você integrou HTML, CSS e JavaScript.🥳";
-}
--- Softex/Softex/AULA2/ATV4/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarSaudacao(){
-    //Pega o parágrafo com id="mensagem"
-    let nome = "Myller";
-    const paragrafo = document.getElementById("mensagem");
-
-    //Altera o conteúdo do parágrafo
-    paragrafo.textContent = "Olá, " + nome + ", seja bem-vindo!🐻";
-}
